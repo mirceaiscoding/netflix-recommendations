bea70af5bbb230afcb11994adc83c70b09228afc
Bina Mircea <bina.mircea2001@gmail.com>
2022-02-19 15:06:19 +0200

    #2 Removed generated code. Created a base app with nav bar using a custom color scheme

---
+++ flutter_app/lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Color scheme for app
+const kPrimaryColor = Color.fromRGBO(246, 199, 0, 1);
+const kBackgroundColor = Color.fromRGBO(23, 21, 21, 1);
+const kTextColor = Color.fromARGB(255, 255, 255, 255);
+const kIconColor = Color.fromARGB(255, 0, 0, 0);
+
+const double kDefaultPadding = 20.0;
---
+++ flutter_app/lib/screens/home/components/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/constants.dart';
+
+class Homescreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        primary: false,
+      ),
+    );
+  }
+}
