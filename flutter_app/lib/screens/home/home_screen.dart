488525b99392217f5122138ce3991527f685741f
Bina Mircea <bina.mircea2001@gmail.com>
2022-02-21 15:24:29 +0200

    #2 Added bottom bar with icons

--- flutter_app/lib/screens/home/home_screen.dart
+++ flutter_app/lib/screens/home/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_app/constants.dart';
 import 'package:flutter_app/screens/home/components/body.dart';
@@ -10,6 +8,55 @@ class Homescreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(),
       body: Body(),
+      bottomNavigationBar: Container(
+        color: kPrimaryColor,
+        height: 80.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              onPressed: nextMovie(),
+              iconSize: 40,
+              icon: const Icon(
+                Icons.arrow_back_ios_outlined,
+                color: kIconColor,
+              ),
+            ),
+            IconButton(
+              onPressed: dislikeMovie(),
+              iconSize: 40,
+              icon: const Icon(
+                Icons.sentiment_dissatisfied_outlined,
+                color: kIconColor,
+              ),
+            ),
+            IconButton(
+              onPressed: addToWatchlist(),
+              iconSize: 50,
+              icon: const Icon(
+                Icons.add_circle,
+                color: kIconColor,
+              ),
+            ),
+            IconButton(
+              onPressed: likeMovie(),
+              iconSize: 40,
+              icon: const Icon(
+                Icons.sentiment_satisfied_outlined,
+                color: kIconColor,
+              ),
+            ),
+            IconButton(
+              onPressed: nextMovie(),
+              iconSize: 40,
+              icon: const Icon(
+                Icons.arrow_forward_ios_outlined,
+                color: kIconColor,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -32,5 +79,25 @@ class Homescreen extends StatelessWidget {
 
   openMenu() {
     // TODO: Open menu
+  }
+
+  dislikeMovie() {
+    // TODO
+  }
+
+  likeMovie() {
+    // TODO
+  }
+
+  nextMovie() {
+    // TODO
+  }
+
+  previousMovie() {
+    // TODO
+  }
+
+  addToWatchlist() {
+    // TODO
   }
 }
