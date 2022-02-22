ea3be6dd3f8c9c9b190164b051d8dacc5ecae008
Troaca Denis <denistroaca@yahoo.com>
2022-02-22 20:19:15 +0200

    #3 Added page view and constructor with parameters
    Swiping between movies works

--- flutter_app/lib/screens/home/components/body.dart
+++ flutter_app/lib/screens/home/components/body.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/constants.dart';
 
 class Body extends StatelessWidget {
-  var movie = {
-    'title': 'Bumblebee',
-    'year': '2018',
-    'score': '6.7',
-    'number_of_reviews': '156k',
-    'description':
-        'On the run in the year 1987, Bumblebee finds refuge in a junkyard in a small California beach town. On the cusp of turning 18 and trying to find her place in the world, Charlie Watson discovers Bumblebee, battle-scarred and broken.',
-  };
+
+  // Constructor
+  // Used like this: Body(movie=...)
+  final movie;
+  const Body({Key? key, @required this.movie }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +32,8 @@ class Body extends StatelessWidget {
                       )),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(kImageBoarderRadius),
-                    child: Image.asset(
-                      "assets/images/bumblebee_poster.jpg", // Movie poster image
+                    child: Image.network(
+                      movie['poster'], // Movie poster image
                     ),
                   ),
                 ),
--- flutter_app/lib/screens/home/home_screen.dart
+++ flutter_app/lib/screens/home/home_screen.dart
@@ -7,7 +7,28 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Body(),
+      body: PageView(
+        children: const <Body>[
+          Body(movie: {
+            'title': 'Bumblebee',
+            'year': '2018',
+            'score': '6.7',
+            'number_of_reviews': '156k',
+            'description':
+                "On the run in the year 1987, Bumblebee finds refuge in a junkyard in a small California beach town. On the cusp of turning 18 and trying to find her place in the world, Charlie Watson discovers Bumblebee, battle-scarred and broken.",
+            'poster' : "https://m.media-amazon.com/images/M/MV5BMjUwNjU5NDMyNF5BMl5BanBnXkFtZTgwNzgxNjM2NzM@._V1_.jpg",
+          }),
+          Body(movie: {
+            'title': 'Cars 3',
+            'year': '2017',
+            'score': '6.7',
+            'number_of_reviews': '91k',
+            'description':
+                "Lightning McQueen sets out to prove to a new generation of racers that he's still the best race car in the world.",
+            'poster' : "https://cdn.europosters.eu/image/750/cars-3-duel-i97645.jpg",
+          }),
+        ],
+      ),
       bottomNavigationBar: Container(
         color: kPrimaryColor,
         height: 80.0,
