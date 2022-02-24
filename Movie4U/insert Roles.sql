d7af0f2d4c5d52e3270e86f12efd187bae1f0aa5
Stefan Grigorescu <grigorescu2001@gmail.com>
2022-02-24 16:20:40 +0200

    included sql file for #4 (insert roles.sql) changed localhost ports (see Properties>launchSettings.json) added Init Migration

---
+++ Movie4U/insert Roles.sql
@@ -0,0 +1,13 @@
+﻿--delete from AspNetRoles;
+
+insert into AspNetRoles
+values('Admin', 'Admin', 'ADMIN', null);
+
+insert into AspNetRoles
+values('BasicUser', 'BasicUser', 'BASICUSER', null);
+
+select * from AspNetRoles;
+
+select * from AspNetUsers;
+
+select * from AspNetUserRoles;
